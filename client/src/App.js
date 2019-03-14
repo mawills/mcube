7da60991254ec8c02b4bd1392c2e85d702f70f59
mawills <wattmills@gmail.com>
2019-03-13 20:02:02 -0400

    Navigation component skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import Navigation from './Navigation';
-import './App.css';
+import Navigation from './components/Navigation';
+import './styles/App.css';
 
 const fakeTabs = [
-  { name: 'tab 1', link: '#', id: 1, selected: true },
-  { name: 'tab 2', link: '#', id: 2 },
-  { name: 'tab 3', link: '#', id: 3 }
+  { label: 'Blog', link: '#', id: 1, selected: true },
+  { label: 'List', link: '#', id: 2 },
+  { label: 'Playtest', link: '#', id: 3 },
+  { label: 'Premium', link: '#', id: 4 },
+  { label: 'Tokens', link: '#', id: 5 },
+  { label: 'Decks', link: '#', id: 6 },
+  { label: 'Analysis', link: '#', id: 7 },
 ];
 
 class App extends Component {
---
+++ client/src/components/Navigation.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import '../styles/Navigation.css';
+
+const NavigationTab = tab => {
+    const { id, label, link, selected } = tab;
+
+    if (selected) {
+        return (
+            <div className={`selected navigation-tab navigation-tab-${id}`}>
+                <a href={link} >{label}</a>
+            </div>
+        )
+    }
+
+    return (
+        <div className={`navigation-tab navigation-tab-${id}`}>
+            <a href={link} >{label}</a>
+        </div>
+    );
+}
+
+const Navigation = ({ tabs }) => {
+    return (
+        <div className="navigation">
+            <div className="navigation-left">
+                <img src="../img/logo.svg" alt="logo" role="presentation" />
+                {tabs.map(tab => (
+                    <NavigationTab {...tab} key={tab.id} />
+                ))}
+            </div>
+            <div className="navigation-right">
+                <a href="#" className="navigation-home-button">
+                    <img src="../img/home.svg" alt="home" role="presentation" />
+                </a>
+                <div className={`navigation-tab navigation-cube-menu`}>
+                    <a href="#" >My Cubes Dropdown</a>
+                </div>
+                <div className={`navigation-tab navigation-sign-in`}>
+                    <a href="#" >Sign In</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navigation;

9881cfa333b5131c7b8d28ccf66a79791a2f2c21
Sam Burba <gitlab@samburba.com>
2019-03-31 13:59:34 -0400

    Use consistent import style

--- server/__tests__/data/cube-store-test.ts
+++ server/__tests__/data/cube-store-test.ts
@@ -3,7 +3,7 @@ import Cube from '../../src/common/cube';
 import FirebaseCubeStore from '../../src/data/firebase-cube-store';
 import InMemoryCubeStore from '../../src/data/in-memory-cube-store';
 import CubeStore from '../../src/data/cube-store';
-import uuid = require('uuid');
+import * as uuid from 'uuid'
 
 const firestoreCubeStoreFactory = () => {
     // Generate a random project id because firestore is used across test suites

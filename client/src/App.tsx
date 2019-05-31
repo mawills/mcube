cc9cccc31fd5afc5e2857389293c9f62824fbf71
mawills <wattmills@gmail.com>
2019-05-30 23:36:59 -0400

    change React import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './components/Home';

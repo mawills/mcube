c22d4b01ed7c63372d137c5e74bb874f1d813382
mawills <wattmills@gmail.com>
2019-05-30 23:12:39 -0400

    style Navigation component

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,57 +1,81 @@
 import React from 'react';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import Toolbar from '@material-ui/core/Toolbar';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
-interface LinkTabProps {
+interface toolbarLink {
     label?: string;
     href?: string;
 }
 
-function LinkTab(props: LinkTabProps) {
-    return (
-        <Tab
-            component="a"
-            onClick={(
-                event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
-            ) => {
-                event.preventDefault();
-            }}
-            {...props}
-        />
-    );
-}
-
+// https://github.com/mui-org/material-ui/blob/master/docs/src/pages/getting-started/page-layout-examples/blog/Blog.js
 const useStyles = makeStyles((theme: Theme) => ({
-    root: {
-        flexGrow: 1,
-        backgroundColor: theme.palette.background.paper,
+    toolbar: {
+        borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    toolbarTitle: {
+        flex: 1,
+    },
+    toolbarSecondary: {
+        justifyContent: 'space-between',
+        overflowX: 'auto',
+    },
+    toolbarLink: {
+        padding: theme.spacing(1),
+        flexShrink: 0,
     },
 }));
 
 function Navigation() {
     const classes = useStyles();
-    const [value, setValue] = React.useState(0);
 
-    function handleChange(event: React.ChangeEvent<{}>, newValue: number) {
-        setValue(newValue);
-    }
+    const toolbarLinks: toolbarLink[] = [
+        { label: 'Blog', href: '/blog' },
+        { label: 'List', href: '/list' },
+        { label: 'Playtest', href: '/playtest' },
+        { label: 'Premium', href: '/premium' },
+        { label: 'Decks', href: '/decks' },
+        { label: 'Analysis', href: '/analysis' },
+    ];
 
     return (
-        <div className={classes.root}>
+        <>
             <AppBar position="static">
-                <div>MCUBE</div>
-                <Tabs value={value} onChange={handleChange}>
-                    <LinkTab label="Blog" href="/blog" />
-                    <LinkTab label="List" href="/list" />
-                    <LinkTab label="Playtest" href="/playtest" />
-                    <LinkTab label="Premium" href="/premium" />
-                    <LinkTab label="Decks" href="/decks" />
-                    <LinkTab label="Analysis" href="/analysis" />
-                </Tabs>
+                <Toolbar className={classes.toolbar}>
+                    <Typography
+                        component="h2"
+                        variant="h5"
+                        align="center"
+                        className={classes.toolbarTitle}
+                    >
+                        MCUBE
+                    </Typography>
+                    <Button variant="outlined" size="small">
+                        Sign In
+                    </Button>
+                </Toolbar>
+                <Toolbar
+                    component="nav"
+                    variant="dense"
+                    className={classes.toolbarSecondary}
+                >
+                    {toolbarLinks.map(({ label, href }) => (
+                        <Link
+                            color="textPrimary"
+                            noWrap
+                            key={label}
+                            href={href}
+                            className={classes.toolbarLink}
+                        >
+                            {label}
+                        </Link>
+                    ))}
+                </Toolbar>
             </AppBar>
-        </div>
+        </>
     );
 }
 
--- client/src/theme.tsx
+++ client/src/theme.tsx
@@ -7,13 +7,13 @@ export default createMuiTheme({
             light: '#ffffff',
             main: '#e0f7fa',
             dark: '#aec4c7',
-            contrastText: '#000000',
+            contrastText: '#212121',
         },
         secondary: {
             light: '#a7c0cd',
             main: '#78909c',
             dark: '#4b636e',
-            contrastText: '#000000',
+            contrastText: '#212121',
         },
     },
 });
